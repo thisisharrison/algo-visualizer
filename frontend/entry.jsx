8bd4a4f2d3232630d2e67e0bf49c477520043934
thisisharrison <harrison.lau@outlook.com>
2020-12-30 16:06:13 +0800

    Initial commit

---
+++ frontend/entry.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { createStore, applyMiddleware } from 'redux';
+// import thunk from 'redux-thunk';
+// import logger from 'redux-logger';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const content = document.getElementById('content');
+    ReactDOM.render(<h1>Hello</h1>, content);
+});
