99e5fdd501d8cbf367ce6ad789fb79c8546e47e4
thisisharrison <harrison.lau@outlook.com>
2021-04-11 01:12:04 +0800

    Style clean up

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ToolbarContainer from './toolbar/toolbar_container';
 import ListContainer from './list/list_container';
-import { Container, Row } from 'react-bootstrap';
 
 const App = () => (
-  <Container>
-    <Row>
+  <div>
       <h1>Sorting Algorithms</h1>
       {/* <h1>Inside App</h1> */}
       <ToolbarContainer />
       <ListContainer />
-    </Row>
-  </Container>
+  </div>
 );
 
 export default App;
